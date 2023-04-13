9250856caec99ba8e01ae7f9e0a897da32fdf1c7
DEVRIESE Florent <fdevriese@groupeseb.com>
2023-04-13 16:51:12 +0200

    1h used, lot of work still to be done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewContactComponent } from './components/new-contact/new-contact.component';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { MenuComponent } from './components/menu/menu.component';
 import { ListComponent } from './components/list/list.component';
 import {HttpClientModule} from '@angular/common/http';
@@ -16,12 +16,13 @@ import {HttpClientModule} from '@angular/common/http';
     MenuComponent,
     ListComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        HttpClientModule,
+        ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -24,4 +24,19 @@ export class ListComponent implements OnInit {
     );
   }
 
+  beautifyPhone(phone: string): string {
+    //do the beautifing, no time here, adding space every 2 chars
+    return phone;
+  }
+
+  deleteContact(contactName: string): void {
+    //Needs to add the possibility to delete the contact with the service. And to identify a contact with an id other than with the name
+    console.log('Delete contact : ' + contactName);
+  }
+
+  editContact(contactName: string): void {
+    //Needs to add the possibility to edit the contact with the service. And to identify a contact with an id other than with the name
+    console.log('Edit contact : ' + contactName);
+  }
+
 }
